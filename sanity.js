159f13bd2f991a53be1033044e1419da39cc0bab
Matheus Duque <masaohc@gmail.com>
2022-08-08 13:38:03 -0300

    update dependencies

--- sanity.js
+++ sanity.js
@@ -1,12 +1,11 @@
-import {
-    createImageUrlBuilder,
-    createClient
-} from 'next-sanity'
+import { createClient } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
+
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    apiVersion: '2021-03-25',
+    apiVersion: '2021-10-21',
     useCdn: process.env.NODE_ENV === 'production',
     token: process.env.SANITY_API_TOKEN,
 }
@@ -14,4 +13,4 @@ export const config = {
 export const sanityClient = createClient(config)
 
 export const urlFor = (source) =>
-    createImageUrlBuilder(config).image(source)
+    imageUrlBuilder(config).image(source)
